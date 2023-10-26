225a87e1f00e8311792330518a2f6154c97c75e3
RazzieBoy <97880341+RazzieBoy@users.noreply.github.com>
2023-10-26 13:47:32 +1300

    Code which lets the user move around and shoot

--- game.py
+++ game.py
@@ -1,3 +1,135 @@
 import pygame
-import sys
+from config import *
+from sys import exit
+import math
 
+pygame.init()
+
+#Creating the window
+screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+
+clock = pygame.time.Clock()
+
+#Loads images
+background = pygame.transform.scale(pygame.image.load("background.png").convert(), (WIN_WIDTH, WIN_HEIGHT))
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.pos = pygame.math.Vector2(PLAYER_START_X, PLAYER_START_Y)
+        self.image = pygame.transform.rotozoom(pygame.image.load("player.png").convert_alpha(), 0, PLAYER_SIZE)
+        self.base_player_image = self.image
+        self.hitbox_rect = self.base_player_image.get_rect(center = self.pos)
+        self.rect = self.hitbox_rect.copy()
+        self.speed = 8
+        self.shoot = False
+        self.shoot_cooldown = 0
+        
+
+    def player_rotation(self):
+        self.mouse_coords = pygame.mouse.get_pos()
+        self.x_change_mouse_player = (self.mouse_coords[0] - self.hitbox_rect.centerx)
+        self.y_change_mouse_player = (self.mouse_coords[1] - self.hitbox_rect.centery)
+        self.angle = math.degrees(math.atan2(self.y_change_mouse_player, self.x_change_mouse_player))
+        self.image = pygame.transform.rotate(self.base_player_image, -self.angle)
+        self.rect = self.image.get_rect(center = self.hitbox_rect.center)
+
+    def user_input(self):
+        self.velocity_x = 0
+        self.velocity_y = 0
+
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            self.velocity_y = -self.speed
+        if keys[pygame.K_s]:
+            self.velocity_y = self.speed
+        if keys[pygame.K_d]:
+            self.velocity_x = self.speed
+        if keys[pygame.K_a]:
+            self.velocity_x = -self.speed
+
+        if self.velocity_x != 0 and self.velocity_y != 0: #moving diagonally
+            self.velocity_x /= math.sqrt(2)
+            self.velocity_y /= math.sqrt(2)
+        
+        if pygame.mouse.get_pressed() == (1, 0, 0) or keys[pygame.K_SPACE]:
+            self.shooy = True
+            self.is_shooting()
+        else:
+            self.shoot - False
+
+    def is_shooting(self):
+        if self.shoot_cooldown == 0:
+            self.shoot_cooldown = 20
+            spawn_bullet_pos = self.pos
+            self.bullet = Bullet(spawn_bullet_pos[0], spawn_bullet_pos[1], self.angle)
+            bullet_group.add(self.bullet)
+            all_sprites_group.add(self.bullet)
+
+
+    def move(self):
+        self.pos += pygame.math.Vector2(self.velocity_x, self.velocity_y)
+        self.hitbox_rect.center = self.pos
+        self.rect.center = self.hitbox_rect.center
+
+    def update(self):
+        self.user_input()
+        self.move()
+        self.player_rotation()
+
+        if self.shoot_cooldown > 0:
+            self.shoot_cooldown -= 1
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y, angle):
+        super().__init__()
+        self.image = pygame.image.load("bullet.png").convert_alpha()
+        self.image = pygame.transform.rotozoom(self.image, 0, 0.05)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.speed = 10
+        self.x_velocity = math.cos(self.angle * (2*math.pi/360)) * self.speed
+        self.y_velocity = math.sin(self.angle * (2*math.pi/360)) * self.speed
+        self.bullet_lifetime = 750
+        self.spawn_time = pygame.time.get_ticks() # gets the specific time that the bullet was created
+    
+    def bullet_movement(self):
+        self.x += self.x_velocity
+        self.y += self.y_velocity
+
+        self.rect.x = int(self.x)
+        self.rect.y = int(self.y)
+
+        if pygame.time.get_ticks() - self.spawn_time > self.bullet_lifetime:
+            self.kill()
+    
+    def update(self):
+        self.bullet_movement()
+
+player = Player()
+
+all_sprites_group = pygame.sprite.Group()
+bullet_group = pygame.sprite.Group()
+
+all_sprites_group.add(player)
+
+while True:
+    keys = pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    screen.blit(background, (0,0))
+
+    all_sprites_group.draw(screen)
+    all_sprites_group.update()
+    # pygame.draw.rect(screen, "red", player.hitbox_rect, width=2)
+    # pygame.draw.rect(screen, "yellow", player.rect, width=2)
+
+    pygame.display.update()
+    clock.tick(FPS)
